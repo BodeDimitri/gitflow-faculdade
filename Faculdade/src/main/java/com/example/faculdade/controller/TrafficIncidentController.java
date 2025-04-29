f535f0e35be372cd95c41a84d2b6c202eb9b9b0d
Diego Sinhorini Albino <diegocesaralbino@gmail.com>
2025-04-29 11:27:24 -0300

    Fix

--- Faculdade/src/main/java/com/example/faculdade/controller/TrafficIncidentController.java
+++ Faculdade/src/main/java/com/example/faculdade/controller/TrafficIncidentController.java
@@ -5,6 +5,12 @@ import com.example.faculdade.repository.TrafficIncidentRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Optional;
+
 
 import java.util.List;
 
