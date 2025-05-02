d42c1d0eca3a2e021dbb3886c1382a089f25731c
Diego Sinhorini Albino <diegocesaralbino@gmail.com>
2025-05-02 13:54:20 -0300

    .

--- Faculdade/src/test/java/com/example/faculdade/CucumberTestRunner.java
+++ Faculdade/src/test/java/com/example/faculdade/CucumberTestRunner.java
@@ -6,10 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
-    glue = "com.example.faculdade.controller",
-    plugin = {"pretty", "summary"},
-    publish = true
+    features = "src/test/resources/features",  
+    glue = "com.example.faculdade",            
+    plugin = {"pretty", "summary"},            
+    publish = true                             
 )
 public class CucumberTestRunner {
 }
